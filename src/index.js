0ad1fb13db22324be573223c6554f881e411b35a
ascotlan <antonio.k.scotland@gmail.com>
2023-06-20 18:51:45 -0400

    converted route handlers to use async/await instead of promise chaining

--- src/index.js
+++ src/index.js
@@ -13,34 +13,31 @@ const port = process.env.PORT || 3000; //Set port to value provided by environme
 app.use(express.json());
 
 //Route handler: Create a user using the "/users" endpoint
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body); // create new user passing in require object
 
   //.save() returns a promise. Save to db
-  user
-    .save()
-    .then((userCreated) => {
-      res.status(201).send(userCreated);
-    })
-    .catch((e) => {
-      res.status(400).send(e.name);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 });
 
 // Route handler: Read all users from the database using the '/users' endpoint
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
   // .find() returns a promise
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((e) => {
-      res.status(500).send(e.name);
-    });
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 });
 
 // Route handler: Read a user with a specific id using the "/users/:id" endpoint
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
 
   // must be a string of 12 bytes or a string of 24 hex characters or an integer
@@ -49,59 +46,69 @@ app.get("/users/:id", (req, res) => {
   }
 
   // .findById() returns a promise
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send("User not found or does not exist!");
-      }
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send("User not found or does not exist!");
+    }
 
-      res.send(user);
-    })
-    .catch((e) => res.status(500).send(e.name));
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e.name);
+  }
 });
 
 //Route handler: Create a task using the "/tasks" endpoint
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body); // create new task passing in the required object
 
   //.save() returns a promise. Save to db
-  task
-    .save()
-    .then((taskCreated) => {
-      //taskCreated the object echoed by the db after saving
-      res.status(201).send(taskCreated);
-    })
-    .catch((e) => {
-      res.status(400).send(e.name);
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 });
 
 // Route handler: Read all tasks from the database using the '/tasks' endpoint
-app.get("/tasks", (req, res) => {
+app.get("/tasks", async (req, res) => {
   //find() returns a promise
-  Task.find({})
-    .then((tasks) => res.send(tasks))
-    .catch((e) => res.status(500).send(e.name));
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 });
 
 // Route handler: Read a task with a specific id using the "/tasks/:id" endpoint
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
   if (!ObjectId.isValid(_id)) {
     return res.status(406).send("Task with that invalid id does not exist!");
   }
 
   // findById() returns a promise
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send("Task not found or does not exist!");
-      }
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send("Task not found or does not exist!");
+    }
 
-      res.send(task);
-    })
-    .catch((e) => res.status(500).send(e.name));
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 });
+
+app.patch('/users:id', async (req, res) => {
+  try{
+    
+  }catch(e){
+
+  }
+})
 
 // listen on the port xxxx at the server
 app.listen(port, () => {
